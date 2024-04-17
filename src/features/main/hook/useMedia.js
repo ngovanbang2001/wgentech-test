b8488afc20758f871216131db8e776a4d815ab80
Ngo Van Bang <ngovanbang2001@gmail.com>
2024-04-18 01:59:36 +0700

    change path

--- src/features/main/hook/useMedia.js
+++ src/features/main/hook/useMedia.js
@@ -4,44 +4,44 @@ const useMedia = () => {
       id: 1,
       name: 'Julian Jameson',
       title: 'Profession',
-      background: '/assets/img/img-1.png',
+      background: './assets/img/img-1.png',
       link: [
-        "/assets/icon/facebook.svg",
-        "/assets/icon/instagram.svg",
-        "/assets/icon/twitter.svg"
+        "./assets/icon/facebook.svg",
+        "./assets/icon/instagram.svg",
+        "./assets/icon/twitter.svg"
       ]
     },
     {
       id: 2,
       name: 'Julian Jameson',
       title: 'Profession',
-      background: '/assets/img/img-2.png',
+      background: './assets/img/img-2.png',
       link: [
-        "/assets/icon/facebook.svg",
-        "/assets/icon/instagram.svg",
-        "/assets/icon/twitter.svg"
+        "./assets/icon/facebook.svg",
+        "./assets/icon/instagram.svg",
+        "./assets/icon/twitter.svg"
       ]
     },
     {
       id: 3,
       name: 'Julian Jameson',
       title: 'Profession',
-      background: '/assets/img/img-3.png',
+      background: './assets/img/img-3.png',
       link: [
-        "/assets/icon/facebook.svg",
-        "/assets/icon/instagram.svg",
-        "/assets/icon/twitter.svg"
+        "./assets/icon/facebook.svg",
+        "./assets/icon/instagram.svg",
+        "./assets/icon/twitter.svg"
       ]
     },
     {
       id: 4,
       name: 'Julian Jameson',
       title: 'Profession',
-      background: '/assets/img/img-4.png',
+      background: './assets/img/img-4.png',
       link: [
-        "/assets/icon/facebook.svg",
-        "/assets/icon/instagram.svg",
-        "/assets/icon/twitter.svg"
+        "./assets/icon/facebook.svg",
+        "./assets/icon/instagram.svg",
+        "./assets/icon/twitter.svg"
       ]
     }
   ]
